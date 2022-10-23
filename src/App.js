98143fb948bfd324bfbe42ba5682c2cd7b36b67e
Abdullah sajid <abdullahsajid521@gmail.com>
2022-10-23 20:22:25 +0500

    update commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import Movies from './Movies'
 
 
 function App() {
-  const[movie,setMovie] = useState('')
-  const[content,setContent] = useState([])
-  const API_KEY = "http://www.omdbapi.com/?apikey=e6226e6d"
+  const [movie, setMovie] = useState('')
+  const [content, setContent] = useState([])
+  const API_KEY = "https://www.omdbapi.com/?apikey=e6226e6d"
 
-  const searchMovie = async (title) =>{
+  const searchMovie = async (title) => {
     const response = await fetch(`${API_KEY}&s=${title}`)
     const data = await response.json()
     setContent(data.Search)
-    console.log(data.Search)
   }
   useEffect(() => {
     searchMovie("the matrix")
-  },[movie])
+  }, [movie])
 
   return (
     <div className="App">
@@ -26,24 +25,24 @@ function App() {
       <div className='searchArea'>
 
         <input type="text" placeholder='search Movie' className='inputArea'
-        value={movie} onChange={(e) => setMovie(e.target.value)}/>
+          value={movie} onChange={(e) => setMovie(e.target.value)} />
 
-        <img className='searchIcon' src='https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg' 
-        alt="" onClick={() => searchMovie(movie)} />
+        <img className='searchIcon' src='https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg'
+          alt="" onClick={() => searchMovie(movie)} />
       </div>
 
-      {content.length > 0 
-      ? (
-        <div className='container'>
-        {content.map((getmovie) => 
+      {content.length > 0
+        ? (
+          <div className='container'>
+            {content.map((getmovie) =>
             (
-              <Movies getmovie={getmovie}/>
+              <Movies getmovie={getmovie} />
             ))}
           </div>
-          ):(
-            <div className='no-moive'>
-              <h2 className='white'>no movie found</h2> 
-            </div> 
+        ) : (
+          <div className='no-moive'>
+            <h2 className='white'>no movie found</h2>
+          </div>
         )}
     </div>
   );
